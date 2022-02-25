97fec4b50606c0eba812a0b65f4115bdc0047f09
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-25 17:10:56 +0530

    Create Rectangle Star Pattern.py

---
+++ Geometrical Shape Patterns/Rectangle Star Pattern.py
@@ -0,0 +1,18 @@
+Code :
+
+row = int(input("Input the number of rows : "))
+col = int(input("Input the number of columns : "))
+
+for i in range(row):
+    for j in range(col):
+        print("*", end="")
+    print()
+
+Output:
+
+******
+******
+******
+******
+******
+******
