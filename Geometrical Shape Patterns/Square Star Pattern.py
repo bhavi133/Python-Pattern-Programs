61389b6505485f6a5dcd6344d04f5bc7bbac0f8e
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-25 17:09:21 +0530

    Create Square Star Pattern.py

---
+++ Geometrical Shape Patterns/Square Star Pattern.py
@@ -0,0 +1,16 @@
+Code : 
+
+num = int(input("Input a number : "))
+
+for row in range(num):
+    for col in range(num):
+        print("*", end="")
+    print()
+    
+Output :
+
+*****
+*****
+*****
+*****
+*****
